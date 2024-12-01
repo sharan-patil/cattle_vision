ee6e59614812a0af967a617b6f74ee953bca2247
Sharan Patil <sharanpatil1999@gmail.com>
2024-12-01 10:49:56 -0800

    message

--- code/pages/9_Contact_Us.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title('Meet the team')
-st.subheader('Team members')
-st.write('Sharan Patil')
-st.write('Udaya Madapuri')
-st.write('Naveen Kundla')
-st.subheader('Project Advisor')
-st.write('Dr. Jerry Gao')
